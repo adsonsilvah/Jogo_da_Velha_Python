d5dd5abca92f8303e460dd5d9854d957afd19889
Adson Henrique <80604595+adsonsilvah@users.noreply.github.com>
2023-06-23 20:50:12 -0300

    criando a tela e a grade usando Tkinter

--- teste.py
+++ teste.py
@@ -1,26 +1,53 @@
 import numpy as np
+from tkinter import *
+import random
 
-jogadorO = ''
-jogadorX = ''
+def verifica_espaco_vazio():
+     
+     espacos = 9
 
-tabuleiro = np.array([['', '', ''],
-                      ['', '', ''],
-                      ['', '', '']])
+     for linhas in range(3):
+          for colunas in range(3):
+               if tabuleiro[linhas][colunas]['text'] != '':
+                    espacos-=1
 
-print(tabuleiro)
-print()
+     if espacos == 0:
+            return False
+     else:
+            return True
 
-while True:
-   
-   for i in range(1, 10):
-    jogadorO = [int(o) for o in input("Vez do jogador O: ").split()]
+def troca_jogador(linha, coluna):
+    pass
+def verifica_vitoria():
+     pass
+def novo_jogo():
+    pass
+def revache():
+     pass
 
-    tabuleiro[jogadorO[0], jogadorO[1]] = 'O'
+janela = Tk()
+janela.title('Jogo da Velha')
+jogadores = ["Jogador X"," jogador O"]
+alternajogador = random.choice(jogadores)
 
-    print(tabuleiro)
+tabuleiro = np.empty((3, 3), dtype=object)
 
-    if(jogadorO != ''):
-        jogadorX = [int(x) for x in input("Vez do jogador X: ").split()]
+label = Label(text='Vez do '+ alternajogador)
+label.pack(side='top')
 
-        tabuleiro[jogadorX[0], jogadorX[1]] = 'X'
-        print(tabuleiro)
+reset_button = Button(text="Revache", command=revache)
+reset_button.pack(side="bottom")
+
+reset_button = Button(text="Novo jogo", command=novo_jogo)
+reset_button.pack(side="bottom")
+
+frame = Frame(janela)
+frame.pack()
+
+for linha in range(3):
+    for coluna in range(3):
+        tabuleiro[linha][coluna] = Button(frame, text="",font=('consolas',40), width=5, height=2,
+                                      command= lambda row=linha, column=coluna: troca_jogador(row,column))
+        tabuleiro[linha][coluna].grid(row=linha,column=coluna)
+        
+janela.mainloop()
